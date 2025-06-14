d2450da4234e114fff1f660413b16f1b144cc93d
Rajat <11173135+malikrajat@users.noreply.github.com>
2025-06-14 19:11:57 +0530

    angular 20
    ng 20

--- lib/rm-image-slider.service.d.ts
+++
@@ -1,9 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class RmImageSliderService {
-    constructor();
-    isBase64(str: string): boolean;
-    base64FileExtension(str: string): string;
-    orderArray(arr?: never[], orderType?: string): never[];
-    static ɵfac: i0.ɵɵFactoryDeclaration<RmImageSliderService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<RmImageSliderService>;
-}
